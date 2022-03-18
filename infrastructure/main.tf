2d51d82357c8b2eba45c25354c0bbb181431d045
Jader Lima <jaderablima@gmail.com>
2022-03-18 17:28:59 -0300

    fix main.ft

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "datalake" {
     bucket = "$(var.base_bucket_name)-$(var.ambiente)-$(var.numero_conta)"
     acl = "private"
     server_side_encryption_configuration {
-        rules{
+        rule{
             apply_server_side_encryption_by_default{
                 sse_algorithm = "AES256"
             }
