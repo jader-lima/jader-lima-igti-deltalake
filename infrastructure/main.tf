93dc18c98b8ccf64ef940512f1ffc90e7e2993d9
Jader Lima <jaderablima@gmail.com>
2022-03-20 08:46:39 -0300

    adding all terraform files, lambda package, glue and spark scripts

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,3 @@
-resource "aws_s3_bucket" "datalake" {
-    bucket = "$(var.base_bucket_name)-$(var.ambiente)-$(var.numero_conta)"
-    acl = "private"
-    server_side_encryption_configuration {
-        rule{
-            apply_server_side_encryption_by_default{
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-
-    tags = {
-        IES = "IGTI",
-        CURSO = "EDC"
-    }
-}
 
 resource "aws_s3_bucket_object" "codigo_spark" {
   bucket = aws_s3_bucket.datalake.id
---
+++ infrastructure/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "dl" {
+    bucket = "$(var.base_bucket_name)-$(var.ambiente)-$(var.numero_conta)"
+    acl = "private"
+
+    tags = {
+        IES = "IGTI",
+        CURSO = "EDC"
+    }
+    server_side_encryption_configuration {
+        rule{
+            apply_server_side_encryption_by_default{
+                sse_algorithm = "AES256"
+            }
+        }
+    }  
+}
