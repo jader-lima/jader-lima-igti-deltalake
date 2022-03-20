155b7f388282aa06334a11f80b5b419da800cd64
Jader Lima <jaderablima@gmail.com>
2022-03-20 08:58:44 -0300

    changing s3.tf file

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,16 +1,17 @@
 resource "aws_s3_bucket" "dl" {
-    bucket = "$(var.base_bucket_name)-$(var.ambiente)-$(var.numero_conta)"
-    acl = "private"
+  bucket = "datalake-jader-igti-edc-tf"
+  acl    = "private"
 
-    tags = {
-        IES = "IGTI",
-        CURSO = "EDC"
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
-    server_side_encryption_configuration {
-        rule{
-            apply_server_side_encryption_by_default{
-                sse_algorithm = "AES256"
-            }
-        }
-    }  
+  }
 }
