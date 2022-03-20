2c12592eeeafbccda0244f1254cab1dbc29c50da
Jader Lima <jaderablima@gmail.com>
2022-03-20 12:12:44 -0300

    changing region, subnet parameter in lambda funcion , bucket name in spark and lambda code

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-jader"
+    bucket = "terraform-state-jader-igti"
     key    = "state/igti/edc/mod1/terraform.tfstate"
     region = "us-west-2"
   }
