adcf130dc60e4aa14299f0a9dcea958ad82d6c19
Jader Lima <jaderablima@gmail.com>
2022-03-20 09:18:59 -0300

    removing acl private from bucket

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,17 +1,10 @@
 resource "aws_s3_bucket" "dl" {
   bucket = "datalake-jader-igti-edc-tf"
-  acl    = "private"
-
+  
   tags = {
     IES   = "IGTI",
     CURSO = "EDC"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  
 }
